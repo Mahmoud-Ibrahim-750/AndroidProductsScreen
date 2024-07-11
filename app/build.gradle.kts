2d71ae9d8ad854c9f095dee3621d93a98a02ec65
Mahmoud Ibrahim <mahmoud.ibrahim.saad.750@gmail.com>
2024-07-11 07:53:41 +0300

    Prepare Project for Work
    * created an dummy clean architecture packages (may be changed later).
    * upgraded the target SDK to the latest (Android 15 - API 35).
    * bumped dependencies versions to the latest:
    -> Kotlin Android Plugin.
    * added the necessary dependencies:
    -> Retrofit & Gson Converter.
    -> Dagger-Hilt.
    -> Google Architecture Components (ViewModel & LiveData).
    -> Kotlin Coroutines & Flow.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,18 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "com.mis.route.androidproductsscreen"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mis.route.androidproductsscreen"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -36,13 +38,37 @@ android {
 }
 
 dependencies {
-
+    // Core Dependencies
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Retrofit & Gson Converter
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    // Dagger-Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler)
+
+    // ViewModel & LiveData
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    kapt(libs.androidx.lifecycle.compiler)
+    // alternately - if using Java8, use the following instead of lifecycle-compiler
+//    implementation("androidx.lifecycle:lifecycle-common-java8:$lifecycle_version")
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Testing Dependencies
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    testImplementation(libs.androidx.core.testing)
+    testImplementation(libs.androidx.lifecycle.runtime.testing)
 }
--- app/src/main/java/com/mis/route/androidproductsscreen/ui/screens/products/MainActivity.kt
+++ app/src/main/java/com/mis/route/androidproductsscreen/ui/screens/products/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.mis.route.androidproductsscreen
+package com.mis.route.androidproductsscreen.ui.screens.products
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mis.route.androidproductsscreen.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
