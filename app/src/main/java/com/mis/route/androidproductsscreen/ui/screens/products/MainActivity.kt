fc84790be1e53ca74723c672352d0772729a2909
Mahmoud Ibrahim <mahmoud.ibrahim.saad.750@gmail.com>
2024-07-12 07:21:09 +0300

    Designed the Products list
    * designed the RecyclerView part which displays the products.

--- app/src/main/java/com/mis/route/androidproductsscreen/ui/screens/products/MainActivity.kt
+++ app/src/main/java/com/mis/route/androidproductsscreen/ui/screens/products/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.mis.route.androidproductsscreen.ui.screens.products
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import com.mis.route.androidproductsscreen.R
+import com.mis.route.androidproductsscreen.data.api.ApiManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
